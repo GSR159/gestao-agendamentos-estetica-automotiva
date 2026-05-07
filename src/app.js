fdf6adee9416b4c7be550a087016fed6f95b27aa
GSR159 <guilhermesalatini123@gmail.com>
2026-05-07 17:30:52 -0300

    fix: corrige nome do clienteRoutes para minusculo

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors    = require('cors');
 const app     = express();
 
-const clienteRoutes      = require('./routes/ClienteRoutes');
+const clienteRoutes      = require('./routes/clienteRoutes');
 const veiculoRoutes      = require('./routes/veiculoRoutes');
 const servicoRoutes      = require('./routes/servicoRoutes');
 const agendamentoRoutes  = require('./routes/agendamentoRoutes');
@@ -10,7 +10,7 @@ const authRoutes         = require('./routes/authRoutes');
 const usuarioRoutes      = require('./routes/usuarioRoutes');
 const relatorioRoutes    = require('./routes/relatorioRoutes');
 const clienteAreaRoutes  = require('./routes/clienteAreaRoutes');
-const funcionarioRoutes  = require('./routes/funcionarioRoutes'); 
+const funcionarioRoutes  = require('./routes/funcionarioRoutes');
 
 app.use(cors());
 app.use(express.json());
@@ -23,6 +23,6 @@ app.use('/auth',         authRoutes);
 app.use('/usuarios',     usuarioRoutes);
 app.use('/relatorios',   relatorioRoutes);
 app.use('/cliente',      clienteAreaRoutes);
-app.use('/funcionarios', funcionarioRoutes); 
+app.use('/funcionarios', funcionarioRoutes);
 
 module.exports = app;
