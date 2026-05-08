8b09b56b6255bce1659370e79928052654fee00c
GSR159 <guilhermesalatini123@gmail.com>
2026-05-07 23:59:53 -0300

    fix: adiciona https na URL do Railway

--- frontend/config.js
+++ frontend/config.js
@@ -1,2 +1,2 @@
 // config.js
-window.API = "gestao-agendamentos-estetica-automotiva-production.up.railway.app";
+window.API = "https://gestao-agendamentos-estetica-automotiva-production.up.railway.app";
