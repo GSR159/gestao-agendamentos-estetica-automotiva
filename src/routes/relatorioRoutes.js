9ce743d142d79b664a9cbd25bfcb4380edd81795
GSR159 <guilhermesalatini123@gmail.com>
2026-05-11 18:06:06 -0300

    fix: relatório e routes

--- src/routes/relatorioRoutes.js
+++ src/routes/relatorioRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router  = express.Router();
-const { obterRelatorios } = require('../controllers/relatoriosController');
+const { obterRelatorios } = require('../controllers/relatorioController');
 
 router.get('/', obterRelatorios);
 
